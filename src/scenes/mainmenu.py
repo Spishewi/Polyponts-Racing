997a4f6019bf900ef37eecf34465a82f03978568
Isteering <romain.blaquart@gmail.com>
2025-01-22 16:36:06 +0100

    border radius

--- src/scenes/mainmenu.py
+++ src/scenes/mainmenu.py
@@ -9,8 +9,8 @@ red = (255, 0, 0)
 orange = (255, 165, 0)
 
 #button dimension and position
-height_button = 80
-width_button = 300
+height_button = 80 #hauteur
+width_button = 300 #largeur
 y_button = 100
 dt_space = 20
 
@@ -22,6 +22,7 @@ class MainMenu(Scene):
         self.title_render = title_font.render("Bienvenue dans le jeu!", True, black )
         self.button_font = button_font
         #init of buttons
+        self.BORDER_RADIUS_BUTTON = 5
         window_width = pygame.display.get_surface().get_width()
         self.button_easy_render = button_font.render("Mode facile", True, black)
         self.button_easy = pygame.Rect(window_width//2-width_button//2, y_button, width_button, height_button)  # Position (x, y), width, height
@@ -29,7 +30,8 @@ class MainMenu(Scene):
         self.button_medium = pygame.Rect(window_width//2-width_button//2, y_button+height_button+dt_space, width_button, height_button)
         self.button_hard_render = button_font.render("Mode difficile", True, black)
         self.button_hard = pygame.Rect(window_width//2-width_button//2, y_button+(height_button+dt_space)*2, width_button, height_button)
-
+        #init of image 
+        self.settings = 
     def event_handler(self, event: pygame.Event, *args: list, **kwargs: dict) -> None:
         ...
     def update(self, dt: float, *args: list, **kwargs: dict) -> None:
@@ -38,23 +40,24 @@ class MainMenu(Scene):
         draw_surface.fill("white")
         
         #draw main title
-        draw_surface.blit(self.title_render)
+        title_width = draw_surface.get_width() //2
+        draw_surface.blit(self.title_render, self.title_render.get_rect(center=(title_width, 30)))
         
 
         #draw button easy
-        pygame.draw.rect(draw_surface, green, self.button_easy)
+        pygame.draw.rect(draw_surface, green, self.button_easy, border_radius=self.BORDER_RADIUS_BUTTON)
         text_button_easy = self.button_easy_render.get_rect(center=self.button_easy.center)
         #Display the text on the button easy 
         draw_surface.blit(self.button_easy_render, text_button_easy)
 
         #draw button medium
-        pygame.draw.rect(draw_surface, orange, self.button_medium)
+        pygame.draw.rect(draw_surface, orange, self.button_medium, border_radius=self.BORDER_RADIUS_BUTTON)
         text_button_medium = self.button_medium_render.get_rect(center=self.button_medium.center)
         #Display the text on the button medium 
         draw_surface.blit(self.button_medium_render, text_button_medium)
 
         #draw button hard
-        pygame.draw.rect(draw_surface, red, self.button_hard)
+        pygame.draw.rect(draw_surface, red, self.button_hard, border_radius=self.BORDER_RADIUS_BUTTON)
         text_button_hard = self.button_hard_render.get_rect(center=self.button_hard.center)
         #Display the text on the button easy 
         draw_surface.blit(self.button_hard_render, text_button_hard)
