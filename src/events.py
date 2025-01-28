77d1f9a271937cf9f78c681ae84020dd6e9cb26c
Spishewi <spishewi@gmail.com>
2025-01-28 13:01:13 +0100

    scene with default arguments

--- src/events.py
+++ src/events.py
@@ -1,5 +1,5 @@
 import pygame
 
 SCENE_CHANGE = pygame.event.custom_type()
-def send_scene_change_event(scene: str, **kwargs: dict):
-    pygame.event.post(pygame.event.Event(SCENE_CHANGE, scene=scene, **kwargs))
+def send_scene_change_event(scene: str, scene_args: dict= {}):
+    pygame.event.post(pygame.event.Event(SCENE_CHANGE, scene=scene, scene_args=scene_args))
