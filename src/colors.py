5bc3e96de74341a0e46f84e3f88258543927a73d
Isteering <romain.blaquart@gmail.com>
2025-03-21 15:17:16 +0100

    colors changed

--- src/colors.py
+++ src/colors.py
@@ -3,13 +3,18 @@ import pygame
 
 BLACK = pygame.Color(0, 0, 0)
 WHITE = pygame.Color(255, 255, 255)
-GREEN = pygame.Color(0, 255, 0)
-RED = pygame.Color(255, 0, 0)
-ORANGE = pygame.Color(255, 165, 0)
+GREEN = pygame.Color(64, 214, 126)
+RED = pygame.Color(214, 10, 37)
+ORANGE = pygame.Color(253, 130, 37)
 GREY = pygame.Color(200, 200, 200)
 BLUE = pygame.Color(0, 0, 255)
 BROWN = pygame.Color(165,42,42)
 RICH_BLACK = pygame.Color(13, 27, 42)
 BACKGROUND_COLOR = pygame.Color(237, 237, 233)
-LIGHT_GREEN = pygame.Color(197, 253, 134)
+LIGHT_GREEN = pygame.Color(156, 222, 125)
 VIVIDBLUE = pygame.Color(3, 102, 214)
+ROSE_QUARTZ = pygame.Color(181, 166, 178)
+CARIBBEAN_CURRENT = pygame.Color(0, 176, 204)
+
+LIGHT_GREEN = CARIBBEAN_CURRENT
+GREY = ROSE_QUARTZ
