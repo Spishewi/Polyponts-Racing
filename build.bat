d5ea68ec92b563ffe9371a322e5649acd477e46e
Spishewi <spishewi@gmail.com>
2025-04-11 12:06:23 +0200

    new build script

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
 pygbag --template ./src/pygbag.tmpl --title Polyponts-Racing --app_name Polyponts-Racing --build ./src
+git add src/build/web/*
+git commit -m "building for web"
 git subtree push --prefix src/build/web origin gh-pages
